34313cf2edc76c12f6d3871057d4941dba867edf
kimYSang <qwdbstkd@naver.com>
2019-08-11 23:25:46 +0900

    dp 문제 풀이

--- PS/BOJ11052.cpp
+++ PS/BOJ11052.cpp
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int n;
-int p[10001];
+int p[1001];
+int cache[1001];
 
-int main() {
-	scanf("%d", &n);
+int card(int value) {
+	if(value == 1) return p[1];
 	
-	for(int i=0; i <= n; ++i){
-		
+	int& ret = cache[value];
+
+	if(ret != -1) return ret;
+
+	ret = p[value];
+	
+	for(int i = 1; i <= value; ++i) {
+		ret = max(ret, card(value-i)+ p[i]);
 	}
 	
+	return ret;
+}
+
+
+int main() {
+	memset(cache, -1, sizeof(cache));
+	scanf("%d", &n);
 	
+	for(int i=1; i <= n; ++i){
+		scanf("%d", &p[i]);
+	}	
+	
+	printf("%d", card(n));
 }
