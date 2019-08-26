8d2c283f439bcea046c7613bbd483a82bb38fdb5
KimYSang <qwdbstkd@naver.com>
2019-08-26 22:27:58 +0900

    2217번 문제 풀이

--- PS/그리디/BOJ2217.cpp
+++ PS/그리디/BOJ2217.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 
 int N;
-int g[10001];
-int ret = 0;
+int g[100001];
 
 int main() {
-	scanf("%d", &N);
+	scanf("%d", &N);	
 	
 	for(int i = 0; i < N; ++i){
 		scanf("%d", &g[i]);
@@ -17,14 +16,13 @@ int main() {
 	
 	sort(g, g+N);
 	
-    long long result = 0;
+    int sum = -1;
+    
     for (int i = 0; i < N; i++) {
-        long long sum = g[i] * (i + 1);
-        if(sum > result)
-            result = sum;
+        sum = max(sum,  g[i] * (N-i));
     }
 
-	printf("%d", result);
+	printf("%d", sum);
 	
 	return 0;
 }
